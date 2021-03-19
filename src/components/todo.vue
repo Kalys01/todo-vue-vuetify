3d727e972d39e5097286c23f3602a6e79d926d9e
Kalys01 <kalysosmon96.g@gmail.com>
2021-03-19 19:39:42 +0600

    start of the project

---
+++ src/components/todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+      
+    }),
+}
+</script>
+
+<style scoped lang="scss">
+  .case {
+    color: rgb(172, 41, 73);
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
 
 Vue.config.productionTip = false
 
 new Vue({
   render: function (h) { return h(App) },
+  vuetify,
 }).$mount('#app')
